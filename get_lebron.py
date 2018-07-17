94004840dbf37d8c44cf706eb989ad88bed7b74f
Praneeth Thalla <pthalla1@gmail.com>
2018-07-16 22:31:13 -0400

    Printed everywhere it says LeBron (A LOT)

--- get_lebron.py
+++ get_lebron.py
@@ -1,10 +1,19 @@
 import requests
 from bs4 import BeautifulSoup as bs
+import re
+
+def find_lebron():
+  print("found")
 
 url = "http://www.espn.com/nba/"
 
 page = requests.get(url)
 soup = bs(page.text, 'html.parser')
 
-output = soup.find_all(text="LeBron")
-print(output)
+#output = soup.find_all(text="LeBron")
+#output = soup.body.findAll(text=re.compile('LeBron'))
+output = soup.find_all(string=re.compile("LeBron"))
+for title in output:
+  print(title)
+#print(output)
+
