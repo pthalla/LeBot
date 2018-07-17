0c2e2c04e1f5d61a5200eec5a244700b54287efe
Praneeth Thalla <pthalla1@gmail.com>
2018-07-16 23:35:39 -0400

    Printed out all the links about LeBron on ESPN

--- get_lebron.py
+++ get_lebron.py
@@ -2,18 +2,20 @@ import requests
 from bs4 import BeautifulSoup as bs
 import re
 
-def find_lebron():
-  print("found")
-
 url = "http://www.espn.com/nba/"
-
 page = requests.get(url)
 soup = bs(page.text, 'html.parser')
 
-#output = soup.find_all(text="LeBron")
-#output = soup.body.findAll(text=re.compile('LeBron'))
-output = soup.find_all(string=re.compile("LeBron"))
-for title in output:
-  print(title)
-#print(output)
+tags = soup.find_all("a", href=re.compile("lebron", re.IGNORECASE))
 
+# Formatting the LeBron references:
+links = []
+for tag in tags:
+  link = tag["href"]
+  if (link[0] == "/"):
+    link = "http://www.espn.com" + link
+  links.append(link)
+
+links = set(links)
+for link in links:
+  print(link)
