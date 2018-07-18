7e1805b120a84d0997c93428a00b4710294965a9
Praneeth Thalla <pthalla1@gmail.com>
2018-07-17 20:16:49 -0400

    Opened up a Twitter API account

--- get_lebron.py
+++ get_lebron.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup as bs
 import re
+import tweepy
 
 url = "http://www.espn.com/nba/"
 page = requests.get(url)
@@ -17,5 +18,18 @@ for tag in tags:
   links.append(link)
 
 links = set(links)
-for link in links:
-  print(link)
+#for link in links:
+#  print(link)
+'''
+auth = tweepy.OAuthHandler("4EIzGMER6zRRHUmDOfBKZ1bRT", "consumer_secret")
+auth.set_access_token("access_token", "access_token_secret")
+
+api = tweepy.API(auth)
+
+public_tweets = api.home_timeline()
+#for tweet in public_tweets:
+#    print(tweet.text)
+
+'''
+print(len("4EIzGMER6zRRHUmDOfBKZ1bRT"))
+print(len("5GAVXHDRYRrWeeFhQGyU3SJhomZdJjP3fEqL7jt94JUsYRrfZ4"))
