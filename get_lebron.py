016e2ff04ac719f21ff752c14dcec0d5ccec39fa
PraneethVT <pthalla1@gmail.com>
2018-07-17 23:08:47 -0400

    Added requirements and set up virtualenv on Windows machine

--- get_lebron.py
+++ get_lebron.py
@@ -1,7 +1,8 @@
 import requests
 from bs4 import BeautifulSoup as bs
 import re
-import tweepy
+from TwitterAPI import TwitterAPI
+import config
 
 url = "http://www.espn.com/nba/"
 page = requests.get(url)
@@ -20,16 +21,7 @@ for tag in tags:
 links = set(links)
 #for link in links:
 #  print(link)
-'''
-auth = tweepy.OAuthHandler("4EIzGMER6zRRHUmDOfBKZ1bRT", "consumer_secret")
-auth.set_access_token("access_token", "access_token_secret")
 
-api = tweepy.API(auth)
-
-public_tweets = api.home_timeline()
-#for tweet in public_tweets:
-#    print(tweet.text)
-
-'''
-print(len("4EIzGMER6zRRHUmDOfBKZ1bRT"))
-print(len("5GAVXHDRYRrWeeFhQGyU3SJhomZdJjP3fEqL7jt94JUsYRrfZ4"))
+api = TwitterAPI(config.api_key, config.api_secret, config.access_token, config.access_token_secret)
+r = api.request("statuses/update", {"status": "This is from my python!"})
+print(r.status_code)
